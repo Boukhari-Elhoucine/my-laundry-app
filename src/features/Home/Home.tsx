b1d60f39e409c3b5206e1d3e3f4353e07a9b2944
OussamaDjebbour <oussama_djebbour@outlook.com>
2024-05-12 20:30:39 +0100

    trial

--- src/features/Home/Home.tsx
+++ src/features/Home/Home.tsx
@@ -3,6 +3,7 @@ import Grid from '@mui/material/Grid/Grid';
 import { HeroSection } from './components/HeroSection';
 
 const Home: React.FC = () => {
+  console.log('Hiiii');
   return (
     <Grid container justifyContent="space-between">
       <HeroSection />
